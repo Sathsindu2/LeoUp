e346dcfb8a59bdacd890c6c71e1f39f9bb163a8a
Sathsindu2 <slcretoe@gmail.com>
2025-07-04 11:43:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function startTimer() {
+    alert("Pomodoro Timer will be added soon!");
+}
+function scrollToTechniques() {
+    document.getElementById("techniques").scrollIntoView({ behavior: 'smooth' });
+}
